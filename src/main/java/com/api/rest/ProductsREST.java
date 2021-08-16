e535f44c8ee7a4ccac1920b716861681af49c20c
creator-krixus <wilsonrueda2@gmail.com>
2021-08-15 19:32:34 -0500

    Second commit

--- src/main/java/com/api/rest/ProductsREST.java
+++ src/main/java/com/api/rest/ProductsREST.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -47,5 +49,24 @@ public class ProductsREST {
 	public ResponseEntity<Product> createProduct(@RequestBody Product product){
 		Product newProduct = productDAO.save(product);
 		return ResponseEntity.ok(newProduct);
+	}
+	
+	@DeleteMapping(value="{productId}")
+	public ResponseEntity<Void> deleteProduct(@PathVariable("productId")Long productId){
+		productDAO.deleteById(productId);
+		return ResponseEntity.ok(null);
+	}
+	
+	@PutMapping
+	public ResponseEntity<Product> updateProduct(@RequestBody Product product){
+		Optional<Product> optionalProduct= productDAO.findById(product.getId());
+		if (optionalProduct.isPresent()) {
+			Product updateProduct = optionalProduct.get();
+			updateProduct.setName(product.getName());
+			productDAO.save(updateProduct);
+			return ResponseEntity.ok(updateProduct);
+		}else {
+			return ResponseEntity.notFound().build();
+		}
 	}
 }
